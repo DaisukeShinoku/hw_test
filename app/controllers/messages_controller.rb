c26d0f6baa7aa2d8066d9b77734a986ef0d10fd2
DaisukeShinoku <shinoku.daisuke.ycu@gmail.com>
2024-01-21 15:32:04 +0900

    登録をTurbo Streamsでやる

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -29,7 +29,7 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
 
     if @message.save
-      redirect_to @message, notice: "メッセージを登録しました。"
+      flash.now.notice = "メッセージを登録しました。"
     else
       render :new, status: :unprocessable_entity
     end
